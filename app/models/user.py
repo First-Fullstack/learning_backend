84faac3f63ebfb52da0e44d746c349b9251d9521
First-Fullstack <firstcol123@gamil.com>
2025-09-20 22:00:45 +0900

    fix-auth-register

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, BigInteger, Identity, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
@@ -8,7 +8,7 @@ from app.db.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(BigInteger, Identity(start=1, increment=1), primary_key=True)
     name = Column(String(100), nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
